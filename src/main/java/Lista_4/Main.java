866372188761fbf51f4b31bd9d7f40ee337cc92a
Nicolas Arthur Raulino Oliveira <37841160-nicolasarthur18@users.noreply.replit.com>
2024-11-17 03:01:09 +0000

    Criado a pasta para colocar listas de exercicios

---
+++ src/main/java/Lista_4/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println("\n\nMEDIADORA\n\n");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Insira o primeiro valor: ");
+      double primeiroValor = sc.nextDouble();
+    System.out.print("Insira o segundo valor: ");
+      double segundoValor = sc.nextDouble();
+    System.out.print("Insira o terceiro valor: ");
+      double terceiroValor = sc.nextDouble();
+
+    double media = (primeiroValor + segundoValor + terceiroValor) / 3;
+
+    System.out.println("\nA média é igual a " + media);
+    sc.close();
+  }
+}
